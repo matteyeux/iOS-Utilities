991c2df4ae3d275dda33c143506f55237415dd9b
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2016-02-18 21:24:43 +0100

    Fixed so it works with Apple TV OTA PBZX

--- pbzx.c
+++ pbzx.c
@@ -3,9 +3,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdint.h>
+// Changelog:
+// 02/17/2016 - Fixed so it works with Apple TV OTA PBZX
 
-
-typedef unsigned long long uint64_t;
+#define PBZX_MAGIC      "pbzx"
 
 int main(int argc, const char * argv[])
 {
@@ -14,15 +16,21 @@ int main(int argc, const char * argv[])
 
     char buffer[1024];
     int fd = 0;
+    int minChunk = 0;
 
     if (argc < 2) { fd  = 0 ;}
     else { fd = open (argv[1], O_RDONLY);
            if (fd < 0) { perror (argv[1]); exit(5); }
          }
 
+    if (argc ==3) {
+        minChunk = atoi(argv[2]);
+        fprintf(stderr,"Starting from Chunk %d\n", minChunk);
+
+        }
 
     read (fd, buffer, 4);
-    if (strncmp(buffer, "pbzx", 4)) { fprintf(stderr, "Can't find pbzx magic\n"); exit(0);}
+    if (memcmp(buffer, PBZX_MAGIC, 4)) { fprintf(stderr, "Can't find pbzx magic\n"); exit(0);}
 
     // Now, if it IS a pbzx
 
@@ -36,6 +44,9 @@ int main(int argc, const char * argv[])
     int i = 0;
     int off = 0;
 
+    int warn = 0 ;
+    int skipChunk = 0;
+
     while (flags &   0x01000000) { // have more chunks
     i++;
     read (fd, &flags, sizeof (uint64_t));
@@ -43,23 +54,32 @@ int main(int argc, const char * argv[])
     read (fd, &length, sizeof (uint64_t));
     length = __builtin_bswap64(length);
 
-    fprintf(stderr,"Chunk #%d (flags: %llx, length: %lld bytes)\n",i, flags,length);
-     
+    skipChunk = (i < minChunk);
+    fprintf(stderr,"Chunk #%d (flags: %llx, length: %lld bytes) %s\n",i, flags,length,
+     skipChunk? "(skipped)":"");
+
     // Let's ignore the fact I'm allocating based on user input, etc..
     char *buf = malloc (length);
     read (fd, buf, length);
 
-   // We want the XZ header/footer if it's the payload, but prepare_payload doesn't have that, 
+   // We want the XZ header/footer if it's the payload, but prepare_payload doesn't have that,
     // so just warn.
-    
 
-    if (strncmp(buf, "\xfd""7zXZ\0", 6))  { fprintf (stderr, "Warning: Can't find XZ header. This is likely not XZ data.\n"); }
+
+    if (memcmp(buf, "\xfd""7zXZ", 6))  { warn++;
+                fprintf (stderr, "Warning: Can't find XZ header. Instead have 0x%x(?).. This is likely not XZ data.\n",
+                        (* (uint32_t *) buf ));
+
+                }
     else // if we have the header, we had better have a footer, too
-    if (strncmp(buf + length -2, "YZ", 2)) { fprintf (stderr, "Warning: Can't find XZ footer. This is bad.\n"); }
-
-    write (1, buf, length);
+    if (strncmp(buf + length -2, "YZ", 2)) { warn++; fprintf (stderr, "Warning: Can't find XZ footer. This is bad.\n"); }
+        if (!warn && !skipChunk)
+        {
+        write (1, buf, length);
+        }
+        warn = 0;
 
     }
 
     return 0;
-} 
+}
