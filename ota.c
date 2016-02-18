30c6e886b2eb9f6a0ceadcc34d7c39ffb5d27d10
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2016-02-18 21:25:29 +0100

    Update ota.c

--- ota.c
+++ ota.c
@@ -4,13 +4,32 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h> // for mkdir
+#include <sys/mman.h> // for mmap
 #include <stdint.h>
+#undef ntohl
+#undef ntohs
+
+
+/**
+ *  Apple iOS OTA unpacker - by Jonathan Levin,
+ *  http://NewOSXBook.com/
+ *
+ *  Free for anyone to use, modify, etc. I won't complain :-), but I'd appreciate a mention
+ *
+ * Changelog: 02/08/16 - Replaced alloca with malloc () (full OTAs with too many files would have popped stack..)
+ *
+ *            02/17/16 - Increased tolerance for corrupt OTA - can now seek to entry in a file
+ *
+ *
+ */
+        uint64_t pos = 0;
+typedef         unsigned int    uint32_t;
 
 #pragma pack(1)
 struct entry
 {
- 
- unsigned int usually_0x10_01_00_00;
+
+ unsigned int usually_0x210_or_0x110;
  unsigned short  usually_0x00_00; //_00_00;
  unsigned int  fileSize;
  unsigned short whatever;
@@ -27,7 +46,7 @@ struct entry
 
 #pragma pack()
 
-uint32_t	
+uint32_t
 swap32(uint32_t arg)
 {
 return (ntohl(arg));
@@ -37,149 +56,185 @@ int g_list = 0;
 int g_verbose = 0;
 char *g_extract = NULL;
 
-void 
+void
 extractFile (char *File, char *Name, uint32_t Size, char *ExtractCriteria)
 {
-	// MAYBE extract file (depending if matches Criteria, or "*").
-	// You can modify this to include regexps, case sensitivity, what not. 
-	// presently, it's just strstr()
+        // MAYBE extract file (depending if matches Criteria, or "*").
+        // You can modify this to include regexps, case sensitivity, what not.
+        // presently, it's just strstr()
 
 
-	if (!ExtractCriteria) return;
-	if ((ExtractCriteria[0] != '*') && ! strstr(Name, ExtractCriteria)) return;
-	
-	// Ok. Extract . This is simple - just dump the file contents to its directory.
-	// What we need to do here is parse the '/' and mkdir(2), etc.
-	
-	char *dirSep = strchr (Name, '/');
-	while (dirSep)
-	{
-		*dirSep = '\0';
-		mkdir(Name,0755);
-		*dirSep = '/';
-		dirSep+=1;
-		dirSep = strchr (dirSep, '/');
-	}
+        if (!ExtractCriteria) return;
+        if ((ExtractCriteria[0] != '*') && ! strstr(Name, ExtractCriteria)) return;
 
-	// at this point we're out of '/'s
-	// go back to the last /, if any
-	
-	if (g_verbose)
-	{
-		fprintf(stderr, "Dumping %d bytes to %s\n", Size, Name);
-	}
-	int fd = open (Name, O_WRONLY| O_CREAT);
-	fchmod (fd, 0644);
-	write (fd, File, Size);
-	close (fd);
+        // Ok. Extract . This is simple - just dump the file contents to its directory.
+        // What we need to do here is parse the '/' and mkdir(2), etc.
+
+        char *dirSep = strchr (Name, '/');
+        while (dirSep)
+        {
+                *dirSep = '\0';
+                mkdir(Name,0755);
+                *dirSep = '/';
+                dirSep+=1;
+                dirSep = strchr (dirSep, '/');
+        }
+
+        // at this point we're out of '/'s
+        // go back to the last /, if any
+
+        if (g_verbose)
+        {
+                fprintf(stderr, "Dumping %d bytes to %s\n", Size, Name);
+        }
+        int fd = open (Name, O_WRONLY| O_CREAT);
+        fchmod (fd, 0644);
+        write (fd, File, Size);
+        close (fd);
 
 
 
 } //  end extractFile
 
-int 
+void showPos()
+{
+        fprintf(stderr, "POS is %lld\n", pos);
+}
+int
 main(int argc ,char **argv)
 {
-	char *filename ="p";
-	int i = 0;
 
-	if (argc < 2) {
-		fprintf (stderr,"Usage: %s [-v] [-l] [-e file] _filename_\nWhere: -l: list files in update payload\n       -e _file: extract file from update payload (use \"*\" for all files)\n", argv[0]);
-		exit(10);
-		}
-	
-	for (i = 1;
-	     i < argc -1;
-	     i++)
-	    {
-		// This is super quick/dirty. You might want to rewrite with getopt, etc..
-		if (strcmp (argv[i], "-l") == 0) { g_list++;} 
-		if (strcmp (argv[i] , "-v") == 0) { g_verbose++;}
-		if (strcmp (argv[i], "-e") == 0) { g_extract = argv[i+1]; i++;}
+        signal (2, showPos);
+        char *filename ="p";
+        int i = 0;
 
-	    }
-	
-	filename =argv[argc-1];
-	//unsigned char buf[4096];
+        if (argc < 2) {
+                fprintf (stderr,"Usage: %s [-v] [-l] [-e file] _filename_\nWhere: -l: list files in update payload\n       -e _file: extract file from update payload (use \"*\" for all files)\n", argv[0]);
+                exit(10);
+                }
 
-	int fd = open (filename, O_RDONLY);
-	if (fd < 0) { perror (filename); exit(1);}
+        for (i = 1;
+             i < argc -1;
+             i++)
+            {
+                // This is super quick/dirty. You might want to rewrite with getopt, etc..
+                if (strcmp (argv[i], "-l") == 0) { g_list++;}
+                if (strcmp (argv[i] , "-v") == 0) { g_verbose++;}
+                if (strcmp (argv[i], "-e") == 0) { g_extract = argv[i+1]; i++;}
 
+            }
 
-#if 0
- 	// Don't need this anymore :-)
+        filename =argv[argc-1];
+        //unsigned char buf[4096];
 
-	// Cheat - actually use 0x00 0x50 0x81 0xb4 as an anchor
-	
-	read (fd, buf, 4096);
-	for (i = 0 ; i < 4096; i++)
-	{
-		if ((buf[i] == 0x00) && (buf[i+1] == 0x50) && (buf[i + 2] == 0x81) && (buf[i+3] == 0xb4))
-		{
-			printf("Got anchor\n");
-			lseek(fd, i-26, SEEK_SET);
-			break;
+        int fd = open (filename, O_RDONLY);
+        if (fd < 0) { perror (filename); exit(1);}
 
-			
-		}
+        // 02/17/2016 - mmap
 
-	}
-	if (i == 4096) { fprintf (stderr, "Unable to find anchor\n"); exit(5);}
-	// Otherwise, we can start reading entries from here:
+        struct stat stbuf;
+        int rc = fstat(fd, &stbuf);
 
-#endif
-	
-	i = 0;
-	int bytes_read = 1;
-
-	struct entry *ent = alloca (sizeof(struct entry));
-	while(bytes_read ){
-	bytes_read = read(fd, (void *) ent, sizeof(struct entry));
-	if (bytes_read <= 0) { // fprintf (stderr, "Error reading next entry or EOF (this could be OK if it's the last entry, @TODO implement == 0 or fstat on filesize..)\n"); 
-			exit(1);}
-	i+= bytes_read;
-
-	uint32_t	size = swap32(ent->fileSize);
-
-	// Get Name (immediately after the entry)
-	char *name = alloca(ntohs(ent->nameLen) + 1);
-	bytes_read = read (fd , (void *) name, ntohs(ent->nameLen));
-	if (bytes_read <= 0) { fprintf (stderr, "Error reading Name. This is not ok\n"); exit(2);}
-
-	i+= bytes_read;
-	name[ntohs(ent->nameLen)] = '\0';
-	if (g_list){ 
-	if (g_verbose) {
-	printf ("Entry @0x%d: UID: %d GID: %d Size: %d (0x%x) Namelen: %d Name: ", i,
-							ntohs(ent->uid), ntohs(ent->gid),
-						     size, size,
-						      ntohs(ent->nameLen));
-	}
-	printf ("%s\n", name);}
+        char *mmapped =  mmap(NULL, // void *addr,
+                              stbuf.st_size ,   // size_t len,
+                              PROT_READ,        // int prot,
+                              MAP_PRIVATE,                //  int flags,
+                              fd,               // int fd,
+                              0);               // off_t offset);
 
 
+        if (mmapped == MAP_FAILED)  { perror ("mmap"); exit(1);}
+        i = 0;
 
-	// Get size (immediately after the name)
-	uint32_t	fileSize = swap32(ent->fileSize);
-	if (fileSize) 
-		{
-			char *file = malloc (fileSize);
-			bytes_read = read (fd, (void *) file, fileSize);
-			// Zero length file sizes are apparently ok..
-			if (bytes_read <= 0) { fprintf (stderr, "Error reading file. This is not ok \n"); exit(3);}
-			i+= bytes_read;
-			if (g_extract) { extractFile(file, name, fileSize, g_extract);}
-			free(file);
-		}
+        struct entry *ent = alloca (sizeof(struct entry));
+
+        while(pos + 3*sizeof(struct entry) < stbuf.st_size) {
+
+        ent = (struct entry *) (mmapped + pos );
+
+        pos += sizeof(struct entry);
+
+        if ((ent->usually_0x210_or_0x110 != 0x210 && ent->usually_0x210_or_0x110 != 0x110 &&
+                ent->usually_0x210_or_0x110 != 0x310) ||
+                ent->usually_0x00_00)
+        {
+                fprintf (stderr,"Corrupt entry (0x%x at pos %llu).. skipping\n", ent->usually_0x210_or_0x110,pos);
+                int skipping = 1;
+
+                while (skipping)
+                {
+                   ent = (struct entry *) (mmapped + pos ) ;
+                   while (ent->usually_0x210_or_0x110 != 0x210 && ent->usually_0x210_or_0x110 != 0x110)
+                   {
+                     // #@$#$%$# POS ISN'T ALIGNED!
+                     pos ++;
+                    ent = (struct entry *) (mmapped + pos ) ;
+                   }
+                   // read rest of entry
+                    int nl = ntohs(ent->nameLen);
+
+                    if (ent->usually_0x00_00 || !nl) {
+                //       fprintf(stderr,"False positive.. skipping %d\n",pos);
+                        pos+=1;
+
+
+                        }
+                    else { skipping =0;
+                           pos += sizeof(struct entry); }
+                }
+
+        }
+
+        uint32_t        size = swap32(ent->fileSize);
+
+// fprintf(stdout," Here - ENT at pos %d: %x and 0 marker is %x namelen: %d, fileSize: %d\n", pos, ent->usually_0x210_or_0x110, ent->usually_0x00_00, ntohs(ent->nameLen), size);
+
+        uint32_t        nameLen = ntohs(ent->nameLen);
+        // Get Name (immediately after the entry)
+        //
+        // 02/08/2016: Fixed this from alloca() - the Apple jumbo OTAs have so many files in them (THANKS GUYS!!)
+        // that this would exceed the stack limits (could solve with ulimit -s, or also by using
+        // a max buf size and reusing same buf, which would be a lot nicer)
+
+
+        // Note to AAPL: Life would have been a lot nicer if the name would have been NULL terminated..
+        // What's another byte per every file in a huge file such as this?
+        // char *name = (char *) (mmapped+pos);
+
+        char *name = malloc (nameLen+1);
+
+        strncpy(name, mmapped+pos , nameLen);
+        name[nameLen] = '\0';
+        //printf("NAME IS %s\n", name);
+
+        pos += ntohs(ent->nameLen);
+        if (g_list){
+        if (g_verbose) {
+        printf ("Entry @0x%d: UID: %d GID: %d Size: %d (0x%x) Namelen: %d Name: ", i,
+                                                        ntohs(ent->uid), ntohs(ent->gid),
+                                                     size, size,
+                                                      ntohs(ent->nameLen));
+        }
+        printf ("%s\n", name);}
+
+        // Get size (immediately after the name)
+        uint32_t        fileSize = swap32(ent->fileSize);
+        if (fileSize)
+                {
+                        if (g_extract) { extractFile(mmapped +pos, name, fileSize, g_extract);}
+                        pos +=fileSize;
+                }
 
 
 
 
-	} // Back to loop
+        free (name);
+
+        } // Back to loop
 
 
-	
 
-	close(fd);
+
+        close(fd);
 
 }
